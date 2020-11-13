5eaa4b856f0d422790b0432929d18d1adedc7960
James Shapiro <jamesshapiro@gmail.com>
2020-11-13 09:19:42 -0800

    Creating pages, link component, navbar and footer, and layout container

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+import Layout from "../components/Layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <Layout>
+    <h1>Hello from Gatsby</h1>
+  </Layout>
+)

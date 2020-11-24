e8a22601b8aa45ff9e944e7096ad6ee791f1f750
James Shapiro <jamesshapiro@gmail.com>
2020-11-24 10:31:33 -0800

    Code Exporter - useStaticQuery

---
+++ tutorial/src/examples/Header.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const ComponentName = () => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          data
+          person {
+            name
+            age
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      <h2>{data.site.siteMetadata.person.name}</h2>
+      <h2>{data.site.siteMetadata.person.age}</h2>
+    </div>
+  )
+}
+
+export default ComponentName
--- tutorial/src/pages/examples.js
+++ tutorial/src/pages/examples.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Header from "../examples/Header"
 
 const examples = () => {
   return (
     <div>
       <h1>hello from examples page</h1>
+      <Header />
     </div>
   )
 }

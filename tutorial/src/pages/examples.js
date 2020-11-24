870b9872003f4a7b50d411fd62a713291fe4e21c
James Shapiro <jamesshapiro@gmail.com>
2020-11-24 11:04:34 -0800

    add PageQuery to page

--- tutorial/src/pages/examples.js
+++ tutorial/src/pages/examples.js
@@ -2,15 +2,40 @@ import React from "react"
 import Header from "../examples/Header"
 import HeaderStatic from "../examples/HeaderStatic"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
+
   return (
     <Layout>
       <h1>hello from examples page</h1>
       <Header />
       <HeaderStatic />
+      <h5>author : {author}</h5>
     </Layout>
   )
 }
+
+export const data = graphql`
+  {
+    site {
+      info: siteMetadata {
+        title
+        description
+        author
+        data
+        person {
+          name
+          age
+        }
+      }
+    }
+  }
+`
 
 export default examples

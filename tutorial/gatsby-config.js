5c23d928506a3da7b9b49d4126bd82f309e71276
James Shapiro <jamesshapiro@gmail.com>
2020-11-24 12:10:19 -0800

    install gatsby-image

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -15,6 +15,8 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {

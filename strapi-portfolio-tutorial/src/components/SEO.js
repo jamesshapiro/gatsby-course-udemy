868298a707f6de23c7237c921081df75287c6596
James Shapiro <jamesshapiro@gmail.com>
2020-11-28 12:38:10 -0800

    add Twitter card functionality

--- strapi-portfolio-tutorial/src/components/SEO.js
+++ strapi-portfolio-tutorial/src/components/SEO.js
@@ -30,6 +30,12 @@ const SEO = ({ title, description, subtitle }) => {
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${description}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Gatsby Title" />
+      <meta name="twitter:text:title" content="Gatsby Text Title" />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="og:title" content="OG Title" />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }

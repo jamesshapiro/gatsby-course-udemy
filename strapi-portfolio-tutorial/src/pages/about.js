bde74de1c777478b41d6ea0b3c4f914d584a0e10
James Shapiro <jamesshapiro@gmail.com>
2020-11-28 10:33:22 -0800

    Add about page

--- strapi-portfolio-tutorial/src/pages/about.js
+++ strapi-portfolio-tutorial/src/pages/about.js
@@ -1,11 +1,56 @@
 import React from "react"
-// import Layout from "../components/Layout"
-// import { graphql } from "gatsby"
-// import Title from "../components/Title"
-// import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+import Title from "../components/Title"
+import Image from "gatsby-image"
+
+const About = ({
+  data: {
+    about: { nodes },
+  },
+}) => {
+  const { image, info, stack, title } = nodes[0]
+
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(item => {
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    about: allStrapiAbout {
+      nodes {
+        info
+        stack {
+          id
+          title
+        }
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default About

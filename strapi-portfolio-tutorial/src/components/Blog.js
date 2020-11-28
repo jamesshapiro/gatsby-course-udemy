8b4e137bf59761b0ffd250ee84a78b2f2180ff5e
James Shapiro <jamesshapiro@gmail.com>
2020-11-28 09:27:14 -0800

    add PropTypes

--- strapi-portfolio-tutorial/src/components/Blog.js
+++ strapi-portfolio-tutorial/src/components/Blog.js
@@ -20,6 +20,14 @@ const Blog = ({ id, title, image, date, category, slug, description }) => {
   )
 }
 
-Blog.propTypes = {}
+Blog.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+}
 
 export default Blog

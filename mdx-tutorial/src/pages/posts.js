4b3abe2bbaafbd103aa4f39e8ac30e527e8f84e2
James Shapiro <jamesshapiro@gmail.com>
2020-11-30 12:39:18 -0800

    Add posts page

--- mdx-tutorial/src/pages/posts.js
+++ mdx-tutorial/src/pages/posts.js
@@ -3,13 +3,43 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-// ...GatsbyImageSharpFluid
+
 const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
-    <>
+    <Layout>
       <Hero />
-    </>
+      <Posts posts={posts} title="all posts" />
+    </Layout>
   )
 }
 
 export default PostsPage
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        excerpt
+        frontmatter {
+          author
+          category
+          date(formatString: "MMMM Do, YYYY")
+          readTime
+          slug
+          title
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`

40ace873c40b228d5857d75523aea59d15421d02
James Shapiro <jamesshapiro@gmail.com>
2020-11-30 12:55:57 -0800

    Single Post JSX

--- mdx-tutorial/src/templates/post-template.js
+++ mdx-tutorial/src/templates/post-template.js
@@ -6,10 +6,33 @@ import Image from 'gatsby-image'
 import Banner from '../components/Banner'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-// ...GatsbyImageSharpFluid
+
 const PostTemplate = () => {
   return <h2>post template</h2>
 }
+
+export const query = graphql`
+  query GetSinglePost($slug: String) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        category
+        date(formatString: "MMMM Do, YYYY")
+        slug
+        readTime
+        author
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      body
+    }
+  }
+`
 
 const Wrapper = styled.section`
   width: 85vw;

481623e7bcbf65780b1f3a308fa372562fa85fe5
James Shapiro <jamesshapiro@gmail.com>
2020-11-30 15:34:58 -0800

    first MDX example

--- mdx-tutorial/root-mdx.js
+++ mdx-tutorial/root-mdx.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 
-const components = {}
+const components = {
+  h3: () => <span>and you thought you had a h3</span>,
+}
 
 export const wrapMDX = ({ element }) => {
   return <MDXProvider components={components}>{element}</MDXProvider>

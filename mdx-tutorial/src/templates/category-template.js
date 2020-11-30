b6a57353d32b04f251f660675c3a2717e594e2ea
James Shapiro <jamesshapiro@gmail.com>
2020-11-30 13:26:51 -0800

    Categories Query

--- mdx-tutorial/src/templates/category-template.js
+++ mdx-tutorial/src/templates/category-template.js
@@ -8,4 +8,32 @@ const CategoryTemplate = () => {
   return <h2>categories template</h2>
 }
 
+export const query = graphql`
+  query GetCategory($category: String) {
+    categories: allMdx(
+      filter: { frontmatter: { category: { eq: $category } } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        excerpt
+        frontmatter {
+          category
+          author
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          slug
+          readTime
+          title
+        }
+      }
+    }
+  }
+`
+
 export default CategoryTemplate

1f458a4461b70e70e64cb85a2cb7309cebf5387f
James Shapiro <jamesshapiro@gmail.com>
2020-11-30 15:29:21 -0800

    add Root MDX

---
+++ mdx-tutorial/root-mdx.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {}
+
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}
